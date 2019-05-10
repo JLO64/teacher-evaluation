8b5287e3fec4509e32199637890c32928cee098d
JLO64 <64JulianLopez@gmail.com>
2019-05-09 22:44:28 -0700

    This was the moment that I finally understood constructors, accessors, and declaring objects.

--- teacher_evaluation.java
+++ teacher_evaluation.java
@@ -5,6 +5,9 @@ import java.util.*;
 
 public class teacher_evaluation
 {
+  private String name;
+  private double avgScore;
+
   public static void main(String[] args)
   {
     Scanner scan = new Scanner(System.in);
@@ -12,12 +15,20 @@ public class teacher_evaluation
     System.out.println("What teacher do you want to evaluate?");
     String teacherName = scan.nextLine();
 
+    teacher_evaluation c = new teacher_evaluation(teacherName);
 
+    System.out.println(c.getName());
   }
 
-  public Teacher()
+  public teacher_evaluation(String teachName)
   {
-    String name = "";
-    double avgScore = 0.0;
+    name = teachName;
+    //double avgScore = 0.0;
   }
+
+  public String getName()
+  {
+    return name;
+  }
+
 }
