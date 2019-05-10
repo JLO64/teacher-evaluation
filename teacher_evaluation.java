4c9faf45fe885d5eb53755069ba2d1d442c6ff22
JLO64 <64JulianLopez@gmail.com>
2019-05-09 23:29:54 -0700

    Added an ArrayList and a For-Each list

--- teacher_evaluation.java
+++ teacher_evaluation.java
@@ -15,18 +15,25 @@ public class teacher_evaluation
     System.out.println("What teacher do you want to evaluate?");
     String teacherName = scan.nextLine();
 
-    teacher_evaluation c = new teacher_evaluation(teacherName);
+    teacher_evaluation cd = new teacher_evaluation(teacherName);
 
-    System.out.println(c.getName());
+    System.out.println(cd.getName());   //the accessor getting the name
+
+    List<teacher_evaluation> teacherList = new ArrayList<teacher_evaluation>();   //creation of a nArrayList of the teacher_evaluation object
+    teacherList.add(cd);    //adding the cd object to the ArrayList teacherList
+
+    for(teacher_evaluation eval : teacherList)    //for-each list that traverses the ArrayList of the teacher_evaluation object
+      System.out.println(eval.getName());
+
   }
 
-  public teacher_evaluation(String teachName)
+  public teacher_evaluation(String teachName)   //a constructor
   {
     name = teachName;
-    //double avgScore = 0.0;
+    avgScore = 0.0;
   }
 
-  public String getName()
+  public String getName()   //an accesor
   {
     return name;
   }
