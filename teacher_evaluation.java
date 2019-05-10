b0c51f609957a8a9ecd6c754fe3b1173a0ed1c79
JLO64 <64JulianLopez@gmail.com>
2019-05-09 21:46:26 -0700

    Fixed naming of the public class.

---
+++ teacher_evaluation.java
@@ -0,0 +1,23 @@
+//Made by Julian Lopez
+
+import java.io.*;
+import java.util.*;
+
+public class teacher_evaluation
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("What teacher do you want to evaluate?");
+    String teacherName = scan.nextLine();
+
+
+  }
+
+  public Teacher()
+  {
+    String name = "";
+    double avgScore = 0.0;
+  }
+}
