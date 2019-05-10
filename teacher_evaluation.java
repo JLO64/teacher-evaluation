784c1507b249b55a94b0300de27094b198d60b61
JLO64 <64JulianLopez@gmail.com>
2019-05-10 13:26:02 -0700

    Added mutato, fixed the constructor used ArrayList

--- teacher_evaluation.java
+++ teacher_evaluation.java
@@ -10,12 +10,14 @@ public class teacher_evaluation
 
   public static void main(String[] args)
   {
-    Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 
     System.out.println("What teacher do you want to evaluate?");
     String teacherName = scan.nextLine();
 
-    teacher_evaluation cd = new teacher_evaluation(teacherName);
+		teacher_evaluation cd = new teacher_evaluation();
+		
+		cd.nameChange(teacherName);
 
     System.out.println(cd.getName());   //the accessor getting the name
 
@@ -23,19 +25,26 @@ public class teacher_evaluation
     teacherList.add(cd);    //adding the cd object to the ArrayList teacherList
 
     for(teacher_evaluation eval : teacherList)    //for-each list that traverses the ArrayList of the teacher_evaluation object
-      System.out.println(eval.getName());
+			System.out.println(eval.getName());
+			
+		System.out.println(teacherList.get(0).getName());
 
   }
 
-  public teacher_evaluation(String teachName)   //a constructor
+  public teacher_evaluation()   //a constructor
   {
-    name = teachName;
+    name = "";
     avgScore = 0.0;
   }
 
-  public String getName()   //an accesor
+  public String getName()   //an accesor (doesn't change anything, only accesses instance variables)
   {
     return name;
-  }
+	}
+	
+	public void nameChange(String newName)	//a mutator (changes instance variable values)
+	{
+		name = newName;
+	}
 
 }
