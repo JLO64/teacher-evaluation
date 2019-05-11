0ea80e80adb56eb1f8d5abf9aef646a6e589d7b2
JLO64 <64JulianLopez@gmail.com>
2019-05-11 09:04:26 -0700

    changed comments added a new class still work same

--- teacher_evaluation.java
+++ teacher_evaluation.java
@@ -5,9 +5,6 @@ import java.util.*;
 
 public class teacher_evaluation
 {
-  private String name;
-  private double avgScore;
-
   public static void main(String[] args)
   {
 		Scanner scan = new Scanner(System.in);
@@ -15,29 +12,35 @@ public class teacher_evaluation
     System.out.println("What teacher do you want to evaluate?");
     String teacherName = scan.nextLine();
 
-		teacher_evaluation cd = new teacher_evaluation();
+		evaluation cd = new evaluation();
 		
 		cd.nameChange(teacherName);
 
     System.out.println(cd.getName());   //the accessor getting the name
 
-    List<teacher_evaluation> teacherList = new ArrayList<teacher_evaluation>();   //creation of a nArrayList of the teacher_evaluation object
+    List<evaluation> teacherList = new ArrayList<evaluation>();   //creation of a nArrayList of the evaluation object
     teacherList.add(cd);    //adding the cd object to the ArrayList teacherList
 
-    for(teacher_evaluation eval : teacherList)    //for-each list that traverses the ArrayList of the teacher_evaluation object
+    for(evaluation eval : teacherList)    //for-each list that traverses the ArrayList of the evaluation object
 			System.out.println(eval.getName());
 			
 		System.out.println(teacherList.get(0).getName());
 
   }
 
-  public teacher_evaluation()   //a constructor
+}
+class evaluation
+{
+  private String name;    //instance variable name that is created everytime the class evaluation is created
+  private double avgScore;
+
+  public evaluation()   //a constructor
   {
     name = "";
     avgScore = 0.0;
   }
 
-  public teacher_evaluation(String newName, double score)   //another constructor highlighting method overloading(same method names with different parameters)
+  public evaluation(String newName, double score)   //another constructor highlighting method overloading(same method names with different parameters)
   {
     name = newName;
     avgScore = score;
@@ -51,7 +54,7 @@ public class teacher_evaluation
   public double getScore()   //another accesor (doesn't change anything, only accesses the instance variable for the score)
   {
     return avgScore;
-	}
+  }
 	
 	public void nameChange(String newName)	//a mutator (changes instance variable values)
 	{
@@ -62,5 +65,4 @@ public class teacher_evaluation
 	{
 		avgScore = score;
 	}
-
 }
