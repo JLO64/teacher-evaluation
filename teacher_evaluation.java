c0f86b8e120ac7eb4bc4889b80721a7e6af21ae1
JLO64 <64JulianLopez@gmail.com>
2019-05-14 08:55:16 -0700

    Added printing of q's and a's and rearanged names

--- teacher_evaluation.java
+++ teacher_evaluation.java
@@ -5,17 +5,17 @@ import java.math.*;
 import java.util.*;
 
 public class teacher_evaluation {
-  private static String studentName = "";
+  private static String teacherName;
+  private static String teacherClass;
   private static List<String> questionList = new ArrayList<String>();
   private static List<evalEntry> teacherList = new ArrayList<evalEntry>(); // creation of an ArrayList of the evalEntry object
 
   public static void main(String[] args) {
     readFile();
 
-    Scanner scan = new Scanner(System.in);
-    System.out.print("What is your name? ");
-    studentName = scan.nextLine();
-
+    teachNameChange();
+    teachClassChange();
+    
     createEntry(teacherList);
 
     printList();
@@ -27,14 +27,7 @@ public class teacher_evaluation {
   {
     evalEntry entry = new evalEntry();
 
-    System.out.print("What teacher do you want to evaluate? ");
-    Scanner scan = new Scanner(System.in);
-    String teacherName = scan.nextLine();
-    entry.teachNameChange(teacherName);
-
-    System.out.print("What class does " + teacherName + " teach? ");
-    String teacherClass = scan.nextLine();
-    entry.teachClassChange(teacherClass);
+    entry.studentNameChange();
 
 		entry.askQuestions(questionList);
 		System.out.println();
@@ -47,39 +40,37 @@ public class teacher_evaluation {
 	{
     for (evalEntry eval : teacherList) // for-each list that traverses the ArrayList of the evalEntry object
       printTeachInfo(eval);
-  }
+  }  
 
-  public static void printEntry(List<evalEntry> teacherList, int entryNum) {
-    System.out.println(teacherList.get(entryNum).getTeachName());
-  }
-
-  public static String getStudentName() // an accesor (doesn't change anything, only accesses instance variables)
-  {
-    return studentName;
-  }
-
-  public static void printTeachInfo(evalEntry entry) {
-    System.out.println("Student: " + getStudentName());
-    System.out.println("Teacher: " + entry.getTeachName());
-    System.out.println("Teacher Class: " + entry.getTeachClass());
+	public static void printTeachInfo(evalEntry entry)
+	{
+    System.out.println("Student: " + entry.getStudentName());
+    System.out.println("Teacher: " + getTeachName());
+    System.out.println("Teacher Class: " + getTeachClass());
     System.out.println("Teacher Average Score: " + entry.getTeachScore());
     System.out.println("Teacher Evaluation: " + entry.getTeachEval());
-  }
+    entry.printResponses(questionList);
+	}
 
-  public static void readFile() {
-    try {
+  public static void readFile()
+  {
+		try
+		{
       Scanner questionScan = new Scanner(new File("questions.txt"));
       while (questionScan.hasNextLine()) {
         String line = questionScan.nextLine();
         questionList.add(line);
       }
-    } catch (FileNotFoundException e) {
+		}
+		catch (FileNotFoundException e)
+		{
       // TODO Auto-generated catch block
       // e.printStackTrace();
     }
   }
 
-  public static void exportInfo() {
+	public static void exportInfo()
+	{
     try
     {
 			PrintStream textF = new PrintStream(new File("student_responces.txt"));
@@ -93,40 +84,59 @@ public class teacher_evaluation {
       //e.printStackTrace();
     }    
   }
-}
 
-class evalEntry // represents a teacher
-{
-  private String teacherName; // instance variable teacherName that is created everytime the class evalEntry is created
-  private String teacherClass;
-	private double avgScore;
-  private static List<String> responces = new ArrayList<String>();
-  private String teacherEvaluation;
-
-  public evalEntry() // a constructor
-  {
-    teacherName = "";
-    teacherClass = "";
-    avgScore = 0.0;
-    teacherEvaluation = "";
-  }
-
-  public evalEntry(String newName, String teachClass, double score) // another constructor highlighting method overloading(same method names with different parameters)
-  {
-    teacherName = newName;
-    avgScore = score;
-    teacherClass = teachClass;
-    teacherEvaluation = "";
-  }
-
-  public String getTeachName() // an accesor (doesn't change anything, only accesses instance variables)
+  public static String getTeachName() // an accesor (doesn't change anything, only accesses instance variables)
   {
     return teacherName;
   }
 
-  public String getTeachClass() // an accesor (doesn't change anything, only accesses instance variables)
+  public static String getTeachClass() // an accesor (doesn't change anything, only accesses instance variables)
   {
     return teacherClass;
+  }
+
+  public static void teachNameChange(String newName) // a mutator (changes instance variable values)
+  {
+    teacherName = newName;
+  }
+
+  public static void teachClassChange(String newclass) // a mutator (changes instance variable values)
+  {
+    teacherClass = newclass;
+  }
+
+  public static void teachNameChange()
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("What is the name of the teacher being evaluated? ");
+    teacherName = scan.nextLine();
+  }
+
+  public static void teachClassChange()
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("What is the class that " + teacherName + " teaches? ");
+    teacherClass = scan.nextLine();
+  }
+}
+
+class evalEntry // represents a student's responce
+{
+	private double avgScore; // instance variable teacherName that is created everytime the class evalEntry is created
+  private static List<String> responces = new ArrayList<String>();
+  private String teacherEvaluation;
+  private static String studentName = "";
+
+  public evalEntry() // a constructor
+  {
+    avgScore = 0.0;
+    teacherEvaluation = "";
+  }
+
+  public evalEntry(double score) // another constructor highlighting method overloading(same method names with different parameters)
+  {
+    avgScore = score;
+    teacherEvaluation = "";
   }
 
   public double getTeachScore() // another accesor (doesn't change anything, only accesses the instance variable for the score)
@@ -139,16 +149,6 @@ class evalEntry // represents a teacher
     return teacherEvaluation;
   }
 
-  public void teachNameChange(String newName) // a mutator (changes instance variable values)
-  {
-    teacherName = newName;
-  }
-
-  public void teachClassChange(String newclass) // a mutator (changes instance variable values)
-  {
-    teacherClass = newclass;
-  }
-
   public void teachScoreChange(double score) // another mutator (changes the instance variable for the average score)
   {
     avgScore = score;
@@ -156,19 +156,35 @@ class evalEntry // represents a teacher
   
   public void teachEvalChange(double score) // a mutator (changes instance variable values)
   {
-    if(score < 1.0)
-    {
-      teacherEvaluation = "Fire him";
-    }
-    if(score >= 1.0)
+    if(score == 1.0)
     {
       teacherEvaluation = "Give him a raise";
     }
+    if(score < 1.0)
+    {
+      teacherEvaluation = "Not a bad teacher";
+    }
+    if(score == 0.0)
+    {
+      teacherEvaluation = "Fire him";
+    }    
+  }
+
+  public String getStudentName() // an accesor (doesn't change anything, only accesses instance variables)
+  {
+    return studentName;
+  }
+
+  public void studentNameChange() // an accesor (doesn't change anything, only accesses instance variables)
+  {
+    System.out.print("\nWhat is the name of the student filling out this form? ");
+    Scanner scan = new Scanner(System.in);
+    studentName = scan.nextLine();
   }
 	
 	public void askQuestions(List<String> questionList)
 	{
-		System.out.println("\nPlease reply with either \"Agree\" or \"Disagree\"");
+		System.out.println("Please reply with either \"Agree\" or \"Disagree\"");
 		for (String question : questionList)
 		{
 			System.out.print(question + " ");
@@ -181,11 +197,11 @@ class evalEntry // represents a teacher
 		for (String input : responces)
 		{
       countEntries++;
-			if(input.equals("Agree"))
+			if(input.equals("Agree") || input.equals("agree"))
 			{
         avgscore = avgscore + 1;
 			}
-			if(input.equals("Disagree"))
+			if(input.equals("Disagree") || input.equals("disagree"))
 			{
 				avgscore = avgscore + 0;
 			}
@@ -193,6 +209,16 @@ class evalEntry // represents a teacher
     avgscore = avgscore /countEntries;
     teachScoreChange(round(avgscore, 2));
     teachEvalChange(avgscore);
+  }
+
+  public void printResponses(List<String> questionList)
+	{
+    int i = 0;
+    for (String question : questionList)
+		{
+      System.out.println(question + " " + responces.get(i));
+      i++;
+		}
   }
 
   public static double round(double value, int places)
